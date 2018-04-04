87f55d8524270f95730bf5ce7da88e173fa1acbf
lsmgeb89 <lsmgeb89@gmail.com>
2018-04-03 21:04:01 -0500

    [CS 6350][ass3] Use Vader to analyze sentiment of tweets

--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 sudo apt install python3-pip
 sudo -H pip install --upgrade pip
 sudo -H pip3 install --upgrade pip
-pip3 install tweepy pyspark googlemaps
+sudo -H pip3 install tweepy pyspark googlemaps nltk twython
 
 export PYSPARK_PYTHON=python3
 
--- spark.py
+++ spark.py
@@ -1,7 +1,12 @@
 import json
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
+nltk.download('punkt')
+nltk.download('vader_lexicon')
 
 # Create a local StreamingContext with four working thread and batch interval of 2 second
 sc = SparkContext(master="local[2]", appName="Tweets Sentiment Analysis")
@@ -11,9 +16,31 @@ ssc.checkpoint("tsa")
 # Create a DStream that will connect to localhost:9001
 data_stream = ssc.socketTextStream("localhost", 9001)
 
+
+# Use Vader to analyze sentiment
+def sentiment_analysis(js):
+    sentences = tokenize.sent_tokenize(js["tweet"])
+    sid = SentimentIntensityAnalyzer()
+
+    total = 0
+    count = 0
+    for sentence in sentences:
+        ss = sid.polarity_scores(sentence)
+        total += ss['compound']
+        count += 1
+    avg = total / count
+
+    if avg >= 0.5:
+        js['sentiment'] = 'positive'
+    elif avg >= -0.5:
+        js['sentiment'] = 'neutral'
+    else:
+        js['sentiment'] = 'negative'
+    return js
+
+
 # transform
-data_stream.map(lambda s: json.loads(s.decode("utf-8")))
-data_stream.pprint()
+data_stream.map(lambda s: json.loads(s)).map(sentiment_analysis).pprint()
 
 # Start the computation
 ssc.start()
